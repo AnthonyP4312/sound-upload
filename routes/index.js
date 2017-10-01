7788322e082bd9182753154dc26843e7948452f2
Anthony Palmer <anthony@motivislearning.com>
2017-10-01 12:14:33 -0400

    awd

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,27 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const path = require('path')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Express'
-  });
-});
+  })
+})
 
 router.post('/upload', (req, res, next) => {
-  if (!req.files) return res.status(400).send('No files were uploaded.');
+  if (!req.files) return res.status(400).send('No files were uploaded.')
   let file = req.files.sound
   if (file.name.endsWith('.ogg')) {
-
     // Use the mv() method to place the file somewhere on your server
-    file.mv(path.join(`/home/pi/node/brave-bot/soundFiles`, `${filename}`), function(err) {
-      if (err)
-      return res.status(500).send(err);
+    file.mv(path.join(`/home/pi/node/brave-bot/soundFiles`, `${file.name}`), function (err) {
+      if (err) { return res.status(500).send(err) }
 
-      res.send('File uploaded!');
+      res.send('File uploaded!')
     })
   } else {
     res.status(400).send('wrong file type moron')
   }
 })
 
-
-module.exports = router;
+module.exports = router
