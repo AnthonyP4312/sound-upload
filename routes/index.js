ce67c66add08e771c60ae154d61b5463bfc1ee7c
Anthony Palmer <anthony@motivislearning.com>
2017-10-04 19:11:29 -0400

    awd

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express')
 var router = express.Router()
 const path = require('path')
+// const fs = require('fs')
+const promisify = require('util').promisify
+// const mm = promisify(require('musicmetadata'))
+const pg = require('pg')
+// const moment = require('moment')
+const _ = require('lodash')
+
+var db = new pg.Client()
+db.connect()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Express'
+    title: 'Express',
+    sounds: querySongs()
   })
 })
 
@@ -26,5 +36,14 @@ router.post('/upload', (req, res, next) => {
     res.status(400).send('wrong file type moron')
   }
 })
+
+async function querySongs () {
+  try {
+    let res = await db.query(`SELECT name FROM sound ORDER BY name`)
+    return res.rows.map(row => row.name)
+  } catch (e) {
+    return 'something broke' + e
+  }
+}
 
 module.exports = router
